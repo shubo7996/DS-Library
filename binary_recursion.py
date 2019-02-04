44fce1f6c5fc9b4ab5e187c99ef298e0b9a1869f
shubo7996 <subhamoy.paul986@outlook.com>
2019-02-04 13:21:30 +0530

    Comment added

--- binary_recursion.py
+++ binary_recursion.py
@@ -6,8 +6,8 @@ def binary_search(arr,val):
 	mid_val = arr[mid]
 
 	if val==mid_val: return True
-	if val<mid_val: return binary_search(arr[:len(arr)//2],val)
-	if val>mid_val: return binary_search(arr[len(arr)//2+1:],val)
+	if val<mid_val: return binary_search(arr[:len(arr)//2],val) #recursive function which takes the first half of the array
+	if val>mid_val: return binary_search(arr[len(arr)//2+1:],val) #recursive function which takes the second half of the array 
 
 if __name__ == '__main__':
 	arr = [34,46,58,62,71,84,90]
