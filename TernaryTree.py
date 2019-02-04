8bef60b7d7b63b7894ca661225bcf6544f04adb1
shubo7996 <subhamoy.paul986@outlook.com>
2019-02-05 01:01:32 +0530

    --del() yet to add--

--- TernaryTree.py
+++ TernaryTree.py
@@ -5,6 +5,7 @@ class Node(object):
 		self.leftNode=None
 		self.rightNode=None
 		self.middleNode=None
+		self.value=None
 
 #trie has 26 children, not memory erffiecient
 #Ternary Search Trees are memory effcient
@@ -13,7 +14,7 @@ class TernaryTree(object):
 	def __init__(self):
 		self.rootNode=None
 	
-	def putItem(self, key, value):
+	def putItem(self,key, value):
 		self.rootNode= self._putItem(self.rootNode,key,value,0)
 	
 	def _putItem(self,node,key,value,index):
@@ -23,23 +24,25 @@ class TernaryTree(object):
 			node=Node(c)
 
 		if c<node.character:
-			node.leftNode=self.putItem(node.leftNode,key,value,index)
+			node.leftNode=self._putItem(node.leftNode,key,value,index)
 		elif c>node.character:
-			node.rightNode=self.putItem(node.rightNode,ket,value,index)
+			node.rightNode=self._putItem(node.rightNode,key,value,index)
 		elif index<len(key)-1:
-			node.middleNode=self.putItem(node.middleNode,key,value,index+1)
+			node.middleNode=self._putItem(node.middleNode,key,value,index+1)
 		else:
 			node.value=value
 
+		return node
+
 	def getItem(self,key):
-		node=self._getItem(self,key)
+		node=self._getItem(self.rootNode,key,0)
 
 		if node==None:
 			return None
 
-		return node
+		return node.value
 
-	def _getItem(self,node, key, index):
+	def _getItem(self,node,key, index):
 		
 		if node==None:
 			return None
@@ -47,21 +50,25 @@ class TernaryTree(object):
 		c=key[index]
 
 		if c<node.character:
-			self._getItem(node.leftChild,key,index)
+			return self._getItem(node.leftNode,key,index)
 		elif c>node.character:
-			self._getItem(node.rightChild,key,index)
+			return self._getItem(node.rightNode,key,index)
 		elif index<len(key)-1:
-			self._getItem(node.middleNode,key,index)
+			return self._getItem(node.middleNode,key,index)
 		else:
-			return Node
+			return node
 
 def main():
 
 	tree=TernaryTree()
+	
 	tree.putItem("Mayukh",200)
 	tree.putItem("Suvankar",600)
 	tree.putItem("Rishabh",500)
 	tree.putItem("Kaustav",300)
+
+	print(tree.getItem("Rishabh"))
+	print(tree.getItem("Suvankar"))
 
 if __name__ == '__main__':
  	main() 
